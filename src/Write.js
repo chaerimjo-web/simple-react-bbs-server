1c2a93412ae48e31ce921a6f7dd54f021f8f9c45
chaerimjo-web <chaerim010710@gmail.com>
2024-11-06 12:48:08 +0900

    함수형 write.js 완성

--- src/Write.js
+++ src/Write.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Axios from "axios";
-import { Navigate, Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
+const Write = ({boardId, isModifyMode, handleCancel})=>{
+  const navigate = useNavigate();
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-export default class Write extends Component {
-  state= {
-    isModifyMode:false,
-    title:'',
-    content:'',
-    redirect:false //주소 변경 상태 추가
-  }
-  write = (e)=>{
+  const write = (e)=>{
     e.preventDefault();
     Axios.post('http://localhost:8000/insert',{
-      title:this.state.title,
-      content:this.state.content
+      title:title,
+      content:content
     })
     .then((res) => {
-      this.setState({
-        redirect:true //true이면 화면전환
+      navigate('/'); //등록완료 후 홈으로 이동
       })
-    })
-    .catch((e)=> {
-      // 에러 핸들링
-      console.log(e);
-    });  
-  }
-  update = (e)=>{
+      .catch((e)=> {
+        // 에러 핸들링
+        console.log(e);
+      });  
+    }
+
+
+  const update = (e)=>{
     e.preventDefault();
     Axios.post('http://localhost:8000/update',{
-      title:this.state.title,
-      content:this.state.content,
-      id:this.props.boardId //수정할 글 번호
+      title:title,
+      content:content,
+      id:boardId //수정할 글 번호
     })
     .then((res) => {
-      this.setState({
-        title:'',
-        content:'',
-        isModifyMode:false
-      });
-      this.props.handleCancel();
+        setTitle('');
+        setContent('');
+        setIsEditMode(false);
+        handleCancel();
+        //navigate('/'); 
       //글 수정 완료후 수정모드 -> false로 변경, 목록 다시 조회, boardID 초기화
     })
     .catch((e)=> {
@@ -49,61 +46,46 @@ export default class Write extends Component {
       console.log(e);
     });  
   }
-  detail = () =>{
+
+  const detail = () =>{
     //글번호에 맞는 데이터 조회, 글 결과를 title, content반영, 수정모드 true    
-    Axios.get(`http://localhost:8000/detail?id=${this.props.boardId}`)
+    Axios.get(`http://localhost:8000/detail?id=${boardId}`)
     .then((res) => {
       const {data} = res;  
-      this.setState({
-        title:data[0].BOARD_TITLE,
-        content: data[0].BOARD_CONTENT,
-        isModifyMode:true
-      })
+      setTitle(data[0].BOARD_TITLE);
+      setContent(data[0].BOARD_CONTENT);
+      setIsEditMode(true);
+
     })
     .catch((e)=> {
       // 에러 핸들링
       console.log(e);
     });     
   }
-  //this.prop.isModifyMode에 변동사항이 생기면 detail 함수 실행, componentDidUpdate 함수로 
+  useEffect(()=>{
+    if(isModifyMode && boardId){
+      detail();
+    }
+  },[isModifyMode,boardId])
 
-  componentDidUpdate(prevProps) {
-    // 수정모드이고 boardId가 변경되었다면, 그 글의 내용조회(detail 함수) 실행
-    if (this.props.isModifyMode && this.props.boardId !== prevProps.boardId) {
-      this.detail();
-    }
-  }
-  componentDidMount() { //true, false 확인
-    if (this.props.isModifyMode) {
-      this.detail();
-    }
-  }
+  const handleChangeTitle = (e)=>setTitle(e.target.value);
+  const handleChangeContent = (e)=>setContent(e.target.value);
 
-  handleChange = (e)=>{
-    this.setState({
-      [e.target.name]:e.target.value //계산된 속성
-    });
-    console.log(this.state);
-  }
-  render() {
-    if(this.state.redirect){
-      return <Navigate to="/"/>;
-    }
-    return (      
-      <Form>
-        <Form.Group className="mb-3" controlId="title">
-          <Form.Label>제목</Form.Label>
-          <Form.Control type="text" name="title" value={this.state.title} placeholder="제목을 입력하세요" onChange={this.handleChange}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="content">
-          <Form.Label>내용</Form.Label>
-          <Form.Control as="textarea" name="content" value={this.state.content} rows={3} onChange={this.handleChange} />
-        </Form.Group>
-        <div className="d-flex gap-1">
-          <Button variant="primary" type="submit" onClick={this.state.isModifyMode ? this.update : this.write}>{this.state.isModifyMode ? '수정완료' : '입력완료'}</Button>
-          <Link to="/" className='btn btn-secondary'>취소</Link>       
-        </div>      
-      </Form>
-    )
-  }
+  return (      
+    <Form>
+      <Form.Group className="mb-3" controlId="title">
+        <Form.Label>제목</Form.Label>
+        <Form.Control type="text" name="title" value={title} placeholder="제목을 입력하세요" onChange={handleChangeTitle}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="content">
+        <Form.Label>내용</Form.Label>
+        <Form.Control as="textarea" name="content" value={content} rows={3} onChange={handleChangeContent} />
+      </Form.Group>
+      <div className="d-flex gap-1">
+        <Button variant="primary" type="submit" onClick={isEditMode ? update : write}>{isEditMode ? '수정완료' : '입력완료'}</Button>
+        <Link to="/" className='btn btn-secondary'>취소</Link>       
+      </div>      
+    </Form>
+  )
 }
+export default Write;
